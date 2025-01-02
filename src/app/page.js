974280044354a93386d82a52698f02fb465edd77
2Sabo3 <pathakvishal50@gmail.com>
2025-01-02 12:01:24 +0530

    colour change

--- src/app/page.js
+++ src/app/page.js
@@ -43,7 +43,7 @@ const styles = {
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#121212',
+    backgroundColor: '#f5eded',
     color: 'white',
   },
   title: {
